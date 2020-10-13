3050386d7733e11aea1a28f7b515ce9d31fa4342
Ido <idok@bu.edu>
2020-10-13 12:21:59 -0400

    still errors in snapshots

--- source/snapshots.R
+++ source/snapshots.R
@@ -8,18 +8,19 @@ snapshots <- function(data) {
   k <- 21 # number of eigenvalues
   
   #Run SVD to compute eigenvalues and eigenvectors
-  C <- (t(cx) * cx) / M #Covariance matrix
+  C <- ((t(cx) * cx) / mx) #Covariance matrix
   svd(C)
-  s <- diag(u)
-  s <- s(1:k)
-  u <- d(1:k)
+ s <- diag(C$s)
+ s <- s(1:k)
+ u <- C$u(1:k)
+
   
   results.covariance <- C
   results.eigenvalues <- s
   results.eigenvectors <- u
   
   # Normalize eigenfunction
-  xefun <- t(results.eigenvectors) * t(cx) / (sqrt(s)/sqrt(mx))
+  xefun <- (t(results.eigenvectors) * t(cx) / (sqrt(s)/sqrt(mx)))
   
   results.eigenfunction <- xefun
 }
