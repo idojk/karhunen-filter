58d250f05aa58ab811930eed9a39148c4c41c1f8
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2020-10-14 19:33:01 -0400

    Update snapshots.R

--- source/snapshots.R
+++ source/snapshots.R
@@ -1,4 +1,6 @@
 snapshots <- function(data) {
+ 
+data<-read.csv(file="Colon.txt", header=TRUE)
   
   # Construct eigenfunctions by using eigenvectors
   cx <- data
@@ -6,20 +8,20 @@ snapshots <- function(data) {
   
   # Compute eigenvectors and eigenvalues of covariance matrix
   k <- 21 # number of eigenvalues
-  
+  M<-matrix(1,mx,mx)
   #Run SVD to compute eigenvalues and eigenvectors
   C <- (t(cx) * cx) / M #Covariance matrix
-  svd(C)
-  s <- diag(u)
-  s <- s(1:k)
-  u <- d(1:k)
+  s<-svd(C)
+  D <- s$d
+  U <- s$u
+  V <- s$v
   
   results.covariance <- C
   results.eigenvalues <- s
   results.eigenvectors <- u
   
   # Normalize eigenfunction
-  xefun <- t(results.eigenvectors) * t(cx) / (sqrt(s)/sqrt(mx))
+  xefun <- t(C) * t(cx) / (sqrt(U)/sqrt(mx))
   
   results.eigenfunction <- xefun
 }
