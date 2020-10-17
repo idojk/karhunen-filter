f74ca051a8948ff4e7388cfcb5357f2c37838830
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2020-10-17 15:59:07 -0400

    Pending dimension
    For diagonal matrix of D, do we want to take all 62 cols which is all tumor sites but only 21 obs of each tumor?

--- source/snapshots.R
+++ source/snapshots.R
@@ -1,21 +1,19 @@
 data<-read.csv(file="Colon.txt", header=TRUE)
 
-snapshot<-function(data,nk) 
+snapshot<-function(data,k) 
 {
   # Construct eigenfunctions by using eigenvectors
   cx <- data
   mx <- dim(cx)[2]
-  
+  k=21
   # Compute eigenvectors and eigenvalues of covariance matrix
-  k <- nk # number of eigenvalues
-
   #Run SVD to compute eigenvalues and eigenvectors
   #in matlab: svd-> s—Singular values,U—Left singular vectors,V-Right singular vectors
   #in R: cvd-> d-singular values, u-left singular vectors, v-right singular vectors
   C <- (t(cx) * cx) / mx
   dc<- svd(C)
-  D <- dc$d
-  U <- dc$u
+  D <- (diag(dc$d))[1:k,] #take row 1 to row k of diagonal matrix of D
+  U <- dc$u[,1:k] # take 1 to k columns of left singular matrix
   V <- dc$v
   
   # Normalize eigenfunction
@@ -24,4 +22,4 @@ snapshot<-function(data,nk)
   results<-list(covariance=C, eigenvalue=D, eigenvector=U, eigenfunction=xefun)
 }
 
-snapshot_result<-snapshot(data,nk=21)
+snapshot_result<-snapshot(data,k=21)
