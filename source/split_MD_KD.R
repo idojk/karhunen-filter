f2cf5b3c0fe9809c817e2480405f8dccc14736d2
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2020-10-23 15:20:47 -0400

    Update split_MD_KD.R

--- source/split_MD_KD.R
+++ source/split_MD_KD.R
@@ -1,26 +1,24 @@
 # Julio Change Detection
-# Github snapshots.R
-# 20201012 Xiaoyang Chen
+# Github split_MD_KD.R
+# 20201020 Xiaoyang Chen
 # Build tree
 
-cx <- data
-mx <- dim(cx)[2] # mx is a dimention: 62 tumor cells
+cx <-as.matrix(data) #data: 2000 gene site, 62 obs from tumor and normal tissue sample
+dim(cx)
+mx <- dim(cx)[2] 
 
-dir<-matrix(0L,nrow=mx,ncol=1) # dir is a 0 matrix
-vrs<-apply(cx,2,var) # calculate variance of each tumor cell
+dir<-matrix(0L,nrow=mx,ncol=1) # dir is a 0 matrix if 62*1
+vrs<-apply(cx,2,var) # calculate variance of each tissue sample in 62 tissues
 v<-max(vrs) # max variance
-mx
-dir
 
-projdata=data*dir # but this projection is 0
+projdata<-cx%*%dir # but this projection is 0
+
+#install.packages("mosaic")
+#projdata<-project(cx,dir)
+
 thresh=0.5
-
-if (projDATA<= thresh) {
-  idx_left<-projDATA
-  }   
-else {
-  idx_right<-projDATA
-  }
+idx_left<-as.integer(projdata<=thresh)
+idx_right<-as.integer(projdata>thresh)
 
 
 
