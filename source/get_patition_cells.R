e55cbd27cb5f3266b89190e4a13a138610e9cf71
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2020-10-27 23:46:04 -0400

    Update get_patition_cells.R

--- source/get_patition_cells.R
+++ source/get_patition_cells.R
@@ -3,26 +3,28 @@
 # 20201024 Xiaoyang Chen
 # two way to get patition cells: get_partition_cells or traverse
 
+
 ###function partitions = get_partition_cells(TEST_DATA, tree, MAX_DEPTH)
-if (nargin<3) {MAX_DEPTH = inf}
+if (nargin<3) {MAX_DEPTH <- Inf}
 # then go to function: partitions = traverse(TEST_DATA,tree,1,MAX_DEPTH);
 
-cx <-as.matrix(data)
 ###function partitions = traverse(TEST_DATA,tree,curr_depth,MAX_DEPTH)
-tree<-data.frame("threshold"=0, "split_dir"=0,"right"=0, "left"=0)
-# tree$threshold
+tree<-data.frame("threshold"=0, "split_dir"=matrix(0,nrow=10,ncol=1),"right"=0, "left"=0)
+partitions=matrix(NA,1:dim(cx)[1],1)
 partitions[1:dim(cx)[1],1] <- tree; 
 if (curr_depth >=MAX_DEPTH){break}
-if (left %in% tree){break}
+if (left %notin% tree){break}
 if (tree$left=0){break}
 
-idx=as.integer((cx%*%tree$split_dir)<=tree$threshold)
+idx=as.integer((cx*(tree$split_dir))<=tree$threshold)
 
-if sum(idx)>0 {
-  lparts<-traverse(cx[idx,:],tree$left,curr_depth+1,MAX_DEPTH);
+if (sum(idx)>0) {
+  lparts<-traverse(cx[idx,:],tree$left,curr_depth+1,MAX_DEPTH)
   partitions[idx]<-lparts
 }
 else {
-  rparts<-traverse(cx[-1*idx,:],tree$left,curr_depth+1,MAX_DEPTH);
+  rparts<-traverse(cx[-1*idx,:],tree$left,curr_depth+1,MAX_DEPTH)
   partitions[idx]<-rparts
-}# define tranverse
+}
+# TBD define tranverse
+
