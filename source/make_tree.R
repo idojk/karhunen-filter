eccfb01f59d8caff33d069dcb6571a343c6d8e9a
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2020-11-29 23:58:04 -0500

    Update make_tree.R

--- source/make_tree.R
+++ source/make_tree.R
@@ -7,25 +7,37 @@
 # data<-read.csv(file="Colon.txt", header=TRUE)
 # cx <-as.matrix(data)
 # mx <- dim(cx)[2]
-# nargin=1
 # DATA<-cx
 
 
 #start
 `%notin%` <- Negate(`%in%`)
 
+# nargin <- function() {
+#   if(sys.nframe()<2) stop("must be called from inside a function")
+#   length(as.list(sys.call(-1)))-1
+# }
+
 ### function [tree,DATA] = make_tree(DATA,split_function,params)
 make_tree<-function(DATA,split_function,params){
+  # define nargin in R
+  #print(match.call())
+  nargin <- length(as.list(match.call())) -1
+
   ### define make tree function
   if (nargin<2){
-    split_function=split_PCA
+    split_function='split_PCA'
     }
   #if (nargin<3){params<-matrix(NaN,1,mx)} # a row vector
   if (nargin<3){params<-data.frame(NaN)}
   if ('MAX_DEPTH' %notin% params){params$MAX_DEPTH=15}
   if ('split_fxn_params' %notin% params){   
-    split_fxn_params$spill =0
-    params$split_fxn_params=split_fxn_params
+    #spill <-0
+    #split_fxn_params<-as.data.frame(spill)
+    split_fxn_params<-data.frame(matrix(ncol = 1, nrow = 1)) #initialize null dataframe, TBD: get rid of this inirial NaN var
+    split_fxn_params$spill<-0
+    params$split_fxn_params<-split_fxn_params
+    #str(params)
   }
   
   #Initialize first node
@@ -33,20 +45,26 @@ make_tree<-function(DATA,split_function,params){
   
   # Create tree
   #[tree, node,DATA] = create_tree(DATA,1:size(DATA,1),split_function,params.indexsetsize,params.split_fxn_params, params.MAX_DEPTH,1,node);
-  a<-create_tree(DATA,1:size(DATA,1),split_function,params.indexsetsize,params.split_fxn_params, params.MAX_DEPTH,1,node)
-  tree$size = node
+  #ct=[tree, node,DATA]
+  #idxs=1:nrow(DATA)
+  #curr_depth=1
+  # str(idxs) is a Integer Vector
+  # idxs[1]
+  ct<-create_tree(DATA,1:nrow(DATA),split_function,params.indexsetsize,params.split_fxn_params, params.MAX_DEPTH,1,node)
+  tree$size <- node
 }
 
 ### define create_tree function
 ### [tree, node,DATA] = create_tree(DATA,1:size(DATA,1),split_function,params.indexsetsize,params.split_fxn_params, params.MAX_DEPTH,1,node);
-create_tree<-function(DATA,1:size(DATA,1),split_function,params.indexsetsize,params.split_fxn_params, params.MAX_DEPTH,1,node)){
+create_tree<-function(DATA,idxs,split_function,params.indexsetsize,params.split_fxn_params, params.MAX_DEPTH,curr_depth,node){
   setwd("H:\\2021Sep\\Julio\\ChangeDetection\\ChangeDetectionR\\")
   # path in package
   source("split_KD.R")
   #source("split_function.R")
   
   # initialize
-  idxs=10;
+  tree<-data.frame(matrix(ncol = 1, nrow = 1)) #initialize null dataframe
+  idxs=10
   tree$idxs = idxs
   tree$left = matrix(NaN,1,mx)
   tree$right = matrix(NaN,1,mx)
@@ -57,8 +75,8 @@ create_tree<-function(DATA,1:size(DATA,1),split_function,params.indexsetsize,par
   tree$idxsmax = max(DATA[idxs,])
   tree$idxsmin = min(DATA[idxs,])
   
-  # Add depth to each node: curr_depth not initialize in matlab code
-  tree$currentdepth = curr_depth - 1;
+  # Add depth to each node
+  tree$currentdepth = curr_depth - 1 
   
   # Add node numbering
   tree$node = node
@@ -66,7 +84,7 @@ create_tree<-function(DATA,1:size(DATA,1),split_function,params.indexsetsize,par
   
   # increase node
   node = node + 1
-  cellinfo = cell(1)
+  cellinfo <-data.frame(matrix(ncol = 1, nrow = 1))
   
   if (curr_depth >= MAX_DEPTH){
   # cellinfo[1] = tree
@@ -80,7 +98,7 @@ create_tree<-function(DATA,1:size(DATA,1),split_function,params.indexsetsize,par
   }
   
   #[idx_left, idx_right, threshold, split_dir, proj_data] = split_function(DATA(idxs,:), split_fxn_params);
-  b<- split_function(DATA(idxs,:), split_fxn_params)
+  b<- split_function(DATA[idxs,], split_fxn_params)
   left_idxs = idxs[idx_left]
   right_idxs = idxs[idx_right]
   
