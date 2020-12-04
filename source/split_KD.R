5a577534ed502a84977e1aa50f86275e2396f2d7
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2020-12-03 23:51:34 -0500

    Update split_KD.R

--- source/split_KD.R
+++ source/split_KD.R
@@ -6,28 +6,34 @@
 #function [left_idxs, right_idxs, threshold, split_dir, proj_data] = split_KD(DATA, params)
 split_KD<- function(DATA, params){
 
-`%notin%` <- Negate(`%in%`)
-if ('spill' %notin% params){params$spill=0}
-dir<-matrix(0L,nrow=mx,ncol=1) # dir is a 0 matrix if n*1
-vrs<-apply(cx,2,var)
-v<-max(vrs)
-
-projDATA<-cx%*%dir
-thresh<-median(projDATA)
+  `%notin%` <- Negate(`%in%`)
+  if ('spill' %notin% params){params$spill=0}
+  dir<-matrix(0L,nrow=mx,ncol=1) # dir is a 0 matrix if n*1
+  vrs<-apply(cx,2,var)
+  v<-max(vrs)
   
-prc<-quantile(projDATA,c((0.5-params$spill)*100, (0.5+params$spill)*100))
-comm_idx  = projDATA>prc[1] & projDATA<prc[2] #returnT or F
-idx_left  = projDATA <= thresh #returnT or F
-idx_right = projDATA > thresh
+  proj_data<-cx%*%dir
+  
+  thresh<-median(proj_data)
+  #proj_dataA<-c(1:100,1)
+  prc<-quantile(proj_data,c(0.5-params$spill, 0.5+params$spill))
+  #prc<-quantile(proj_data,c(0.25,0.75))
+  comm_idx  = as.numeric(proj_data>prc[1] & proj_data<prc[2]) # T or F -> 1 or 0
+  idx_left  = proj_data <= thresh #return T or F
+  idx_right = proj_data > thresh
+  
+  split_dir<- dir
+  threshold<- thresh
+  left_idxs = as.numeric(idx_left | comm_idx) 
+  right_idxs = as.numeric(idx_right | comm_idx)
 
-split_dir<- dir
-threshold<- thresh
-left_idxs = idx_left | comm_idx #return T or F
-right_idxs = idx_right | comm_idx
-proj_data <- projDATA
+  # a<-list(left_idxs, right_idxs, threshold, split_dir, proj_data)
+  # str(a)
+  # names(a)
+  # return(as.data.frame(left_idxs, right_idxs, threshold, split_dir, proj_data))
+  return(list("left_idxs"=left_idxs, "right_idxs"=right_idxs, "threshold"=threshold, 
+              "split_dir"=split_dir, "proj_data"=proj_data))
 
-split_KD<-list(left_idxs, right_idxs, threshold, split_dir, proj_data)
-return(split_KD)
 }
 
 
