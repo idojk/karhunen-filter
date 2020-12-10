828c822a6fc3fc8dbcaa01f2554a46975dc4cdd1
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2020-12-09 23:50:35 -0500

    Update split_KD.R

--- source/split_KD.R
+++ source/split_KD.R
@@ -2,25 +2,49 @@
 # Github split_KD.R
 # 20201115 Xiaoyang Chen
 
+#simulation settings
+# setwd("H:\\2021Sep\\Julio\\ChangeDetection\\ChangeDetectionR\\")
+# real data
+# data<-read.csv(file="Colon.txt", header=TRUE)
+
+# simulate data
+# cx <-as.matrix(data)
+# mx <- dim(cx)[2]
+# DATA<-t(cx)
+# mx is a dimention: 2000 gene site, 62 obs from tumor and normal tissue sample
 
 #function [left_idxs, right_idxs, threshold, split_dir, proj_data] = split_KD(DATA, params)
+#                                                              split_function(DATA[idxs,], split_fxn_params)
 split_KD<- function(DATA, params){
 
   `%notin%` <- Negate(`%in%`)
   if ('spill' %notin% params){params$spill=0}
-  dir<-matrix(0L,nrow=mx,ncol=1) # dir is a 0 matrix if n*1
-  vrs<-apply(cx,2,var)
-  v<-max(vrs)
+  #dir<-matrix(0L,nrow=ncol(DATA),ncol=1) # dir is a 0 matrix if n*1
+  dir<-matrix(0,nrow=ncol(DATA),ncol=1)
+  # str(dir)
+  vrs<-apply(DATA,2,var) #return dimention of each col
+  # https://www.mathworks.com/help/matlab/ref/var.html#bum85ue
+  # testmatrix<-matrix(c(4,-2,1,9,5,7),nrow=2, ncol=3,byrow=TRUE)
+  # testmatrix
+  # vrst<-apply(testmatrix,2,var)
+  # apply(testmatrix,2,var) == var(testmatrix,0,1)
+  # vrst
+  v<-which.max(vrs)
+  dir[v,1]<-1
+  #should be dir[v,1]<-1
   
-  proj_data<-cx%*%dir
+  proj_data<-DATA%*%dir
   
   thresh<-median(proj_data)
   #proj_dataA<-c(1:100,1)
   prc<-quantile(proj_data,c(0.5-params$spill, 0.5+params$spill))
   #prc<-quantile(proj_data,c(0.25,0.75))
-  comm_idx  = as.numeric(proj_data>prc[1] & proj_data<prc[2]) # T or F -> 1 or 0
-  idx_left  = proj_data <= thresh #return T or F
-  idx_right = proj_data > thresh
+  comm_idx  = as.numeric(proj_data>prc[1] & proj_data<prc[2]) # common index should be 0 because either left or right
+  idx_left  = as.numeric(proj_data <= thresh) #return T or F
+  idx_right = as.numeric(proj_data > thresh)
+  # sum(comm_idx)
+  # sum(idx_left)
+  # sum(idx_right)
   
   split_dir<- dir
   threshold<- thresh
@@ -35,5 +59,6 @@ split_KD<- function(DATA, params){
               "split_dir"=split_dir, "proj_data"=proj_data))
 
 }
+
 
 
