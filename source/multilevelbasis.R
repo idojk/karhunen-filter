7e43cea214cc92d246ffede026b7a658785314de
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2020-12-22 22:56:40 -0500

    Update multilevelbasis.R

--- source/multilevelbasis.R
+++ source/multilevelbasis.R
@@ -2,6 +2,10 @@
 # multilevelbasis.m
 # 20201221 Xiaoyang Chen
 
+#simulate data
+# ind<-1:63
+
+
 # function [transformcell, ind, datacell, datalevel] = multilevelbasis(tree,coords,degree,polymodel);
 multilevelbasis<-function(tree,coords,degree,polymodel){
 
@@ -11,13 +15,43 @@ multilevelbasis<-function(tree,coords,degree,polymodel){
 
 # Change tree format 
 # [leftchild, rightchild, parent, datacell, datalevel] = convertbintreetolist(tree);
+#return(list('leftchild'=leftchild,'rightchild'=rightchild,'parent'=parent,'datacell'=datacell,'datalevel'=datalevel,'node'=node))
   cl<-convertbintreetolist(tree)
   
   # Sort datalevel
-  [sortdatalevel, ind] <- sort(datalevel)
-  [dummy,unsortkey] = sort(ind);
-  currentlevel = sortdatalevel(end); 
-  levelcounter = size(sortdatalevel,1);
+  tmpdatalevel<- sort(datalevel,return.index=TRUE)
+  sortdatalevel<-tmpdatalevel$x
+  ind<-tmpdatalevel$ix
+  
+  [dummy,unsortkey] = sort(ind)
+  tmpind<-sort(inc,return.index=TRUE)
+  dummy<-tmpind$x
+  unsortkey<-tmpind$ix
+  
+  currentlevel<-tail(sortdatalevel,n=1)
+  levelcounter<-nrow(sortdatalevel)
+  
+
+  # Data
+  #transformcell = cell(length(ind),6);
+  transformcell<-matrix(NaN,length(ind),6)
+  
+  
+  # Process finest level first
+  while (  sortdatalevel(levelcounter) == currentlevel  ) {
+    llb<-leaflocalbasis(datacell{ind(levelcounter)}, coords, polymodel,degree)
+    transformcell[levelcounter,1]<-llb$Scfun
+    transformcell[levelcounter,2]<-llb$Wavefun
+    transformcell[levelcounter,3]<-llb$Cwave
+    transformcell[levelcounter,4]<-llb$Dwave
+    
+    levelcounter=levelcounter - 1
+  }
+  
+ 
+  minlevel = 1
+  
+  
   
   return(list('transformcell'=transformcell,'ind'=ind, 'datacell'=datacell, 'datalevel'=datalevel))
 }
