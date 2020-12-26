758e46470c828d363db473de74a1b454e9fad102
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2020-12-25 23:58:41 -0500

    Update convertbintreetolist.R

--- source/convertbintreetolist.R
+++ source/convertbintreetolist.R
@@ -7,11 +7,14 @@
 
 
 # function  [leftchild,rightchild,parent,datacell,datalevel] = convertbintreetolist(tree);
+# Invoke: cl<-convertbintreetolist(mt$tree)
+
 # Transform tree to list data
 convertbintreetolist<-function(tree){
   
   # Find tree size
-  sizeoftree<-tree$size
+  # sizeoftree<-tree$size
+   sizeoftree=63
   
   # Initialize cell array
   leftchild<-rep(0,sizeoftree)
@@ -29,39 +32,74 @@ convertbintreetolist<-function(tree){
   
   #[leftchild,rightchild,parent,datacell,datalevel,node] = createdata(leftchild,rightchild,parent,datacell,tree,datalevel,node);
   createdataout<-createdata(leftchild,rightchild,parent,datacell,tree,datalevel,node)
+  return(list('leftchild'=leftchild,'rightchild'=rightchild,'parent'=parent,'datacell'=datacell,'datalevel'=datalevel,'node'=node))
+  
 }
 
 
 #function [leftchild,rightchild,parent,datacell,datalevel,node] = createdata(leftchild,rightchild,parent,datacell,tree,datalevel,node);
 createdata<-function(leftchild,rightchild,parent,datacell,tree,datalevel,node){
-
-datacell[mt$tree$node + 1]<- mt$tree$idxs
-datalevel[mt$tree$node + 1]<- mt$tree$currentdepth
-
-if (is.null(mt$tree$left) != TRUE){
-  leftchild[mt$tree$node + 1]<- mt$tree$childleft
+  # formalArgs(createdata)[5]  get only the name of arguments, not the value
+  # datacell[mt$tree$node + 1]<- mt$tree$idxs
+  # datalevel[mt$tree$node + 1]<- mt$tree$currentdepth
+  datacell[node + 1]<- list(tree$idxs)
+  datalevel[node + 1]<- tree$currentdepth
+  # str(mt$tree$idxs)
+  # str(datacell[node + 1])
+  
+  # if is not empty
+  if (is.null(tree$left) != TRUE){
+    #leftchild[mt$tree$node + 1]<- mt$tree$childleft
+    leftchild[node + 1]<- tree$childleft
+    }
+  
+  if (is.null(tree$right) != TRUE){
+    #rightchild[mt$tree$node + 1]<- mt$tree$childright
+    rightchild[node + 1]<- tree$childright
+    }
+  
+  if (node>0) {
+    # parent[mt$tree$node + 1]<-mt$tree$parentnode
+    parent[node + 1]<-tree$parentnode
+    }
+  
+  
+  # node control the flow
+  node = node + 1
+  
+  # if both child is null
+  if (  (is.null(tree$left)) && (is.null(tree$right)) ){
+    return(list('leftchild'=leftchild,'rightchild'=rightchild,'parent'=parent,'datacell'=datacell,'datalevel'=datalevel,'node'=node))
   }
-
-if (is.null(mt$tree$right) != TRUE){
-  rightchild[mt$tree$node + 1]<- mt$tree$childright
-  }
-
-if (node>0) {
-  parent[mt$tree$node + 1]<-mt$tree$parentnode
-  }
-
-
-node = node + 1
-
-# if both child is null
-if (  (is.null(mt$tree$left)) && (is.null(mt$tree$right)) ){
-  return(list(   ))
+  
+  # if (node==63) {
+  #   return(list('leftchild'=leftchild,'rightchild'=rightchild,'parent'=parent,'datacell'=datacell,'datalevel'=datalevel,'node'=node))
+  # }
+  
+  # [leftchild,rightchild,parent,datacell,datalevel,node] = createdata(leftchild,rightchild,parent,datacell,tree.left,datalevel,node);
+  # [leftchild,rightchild,parent,datacell,datalevel,node] = createdata(leftchild,rightchild,parent,datacell,tree.right,datalevel,node);
+  createdataleft<-createdata(leftchild,rightchild,parent,datacell,tree$left,datalevel,node)
+  leftchild<-createdataleft$leftchild
+  rightchild<-createdataleft$rightchild
+  parent<-createdataleft$parent
+  datacell<-createdataleft$datacell
+  # tree$left<-createdataleft$tree$left
+  datalevel<-createdataleft$datalevel
+  node<-createdataleft$node
+  # go over left first before go to right
+  
+  createdataright<-createdata(leftchild,rightchild,parent,datacell,tree$right,datalevel,node)
+  leftchild<-createdataleft$leftchild
+  rightchild<-createdataleft$rightchild
+  parent<-createdataleft$parent
+  datacell<-createdataleft$datacell
+  # tree$right<-createdataleft$tree$right
+  datalevel<-createdataleft$datalevel
+  node<-createdataleft$node
+  
+  
+  #return(list('leftchild'=leftchild,'rightchild'=rightchild,'parent'=parent,'datacell'=datacell,'datalevel'=datalevel,'node'=node))
+ 
 }
 
-
-# [leftchild,rightchild,parent,datacell,datalevel,node] = createdata(leftchild,rightchild,parent,datacell,tree.left,datalevel,node);
-# [leftchild,rightchild,parent,datacell,datalevel,node] = createdata(leftchild,rightchild,parent,datacell,tree.right,datalevel,node);
-createdataleft<-createdata(leftchild,rightchild,parent,datacell,tree.left,datalevel,node)
-createdataright<-createdata(leftchild,rightchild,parent,datacell,tree.right,datalevel,node)
-}
 
