003d62f9a4ec1c801dba57160ece1f6dddfcdc3d
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2021-01-04 23:04:25 -0500

    Update leaflocalbasis.R

--- source/leaflocalbasis.R
+++ source/leaflocalbasis.R
@@ -3,12 +3,13 @@
 # 20201222 Xiaoyang Chen
 
 #function [Scfun, Wavefun, Cwave, Dwave] = leaflocalbasis(datapoints, coords, polymodel,degree)
+# invoke:leaflocalbasis(cl$datacell[[ind[levelcounter]]], coords, polymodel,degree)
 leaflocalbasis<-function(datapoints, coords, polymodel,degree){
   
   # Create local Hierarchical Basis with vanishing moments
   # with respect to the polynomial model 
   
-  coords<-coords[datapoints,] #depends on data structure
+  coords<-as.matrix(coords[datapoints,]) #depends on data structure
   n<-nrow(coords)
   m<-ncol(coords)
   
@@ -16,15 +17,20 @@ leaflocalbasis<-function(datapoints, coords, polymodel,degree){
   # Initial basis 
   V<- diag(n)
   
+  # start here
   # Create local momement matrix
-  Q<- PolynomialMonomials(degree, coords, polymodel, datapoints);
+  setwd("H:/2021Sep/Julio/ChangeDetection/ChangeDetectionR")
+  source('PolynomialMonomials.R')
+  Q<- PolynomialMonomials(degree, coords, polymodel, datapoints)
   M<- t(Q)%*%V
   
   # obtain HB basis
   #[Scfun, Wavefun, Cwave, Dwave] = computewave(M,V,n); 
+  source('computewave.R')
   cw<-computewave(M,V,n)
   return(list('Scfun'=cw$Scfun, 'Wavefun'=cw$Wavefun, 'Cwave'=cw$Cwave, 'Dwave'=cw$Dwave))
 }
+
 
 
 
