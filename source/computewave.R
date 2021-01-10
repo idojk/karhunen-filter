23fb076e3637f7449c65c574d4479cc1aa7c8dcf
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2021-01-09 23:42:08 -0500

    Update computewave.R

--- source/computewave.R
+++ source/computewave.R
@@ -7,25 +7,29 @@ computewave<-function(M,V,n){
   
   # Compute scaling functions and HB
   # from momemnt matrix [U,D,G]=[U,S,V]in matlab = [u,d,v]in R
-  svdm<- svd(M)
-  D<-svdm$d
-  G<-svdm$v
+  # [U,D,G] = svd(M);
+  # https://stackoverflow.com/questions/41972419/different-svd-result-in-r-and-matlab
+  svdm<- svd(M,nu=nrow(M),nv=ncol(M)) #nu nv to require a full svd
+  D<-diag(svdm$d) 
+  G<-svdm$v 
+  U<-svdm$u 
+  
   
   # Construct scaling functions
   numofscalingfunctions <- sum( diag(D) !=0 )
-  Cwave <- G[,1:numofscalingfunctions]
+  Cwave <- G[ ,1:numofscalingfunctions]
   Scfun <- V%*%Cwave
   
   # Construct multilevel basis (wavelets)
   if (n > numofscalingfunctions){
   numofwavelets = n - numofscalingfunctions
-  Dwave <- G[  , numofscalingfunctions + 1: ]
+  Dwave <- G[ ,(numofscalingfunctions + 1):ncol(G)]
   Wavefun <- V%*%Dwave
   } else {
   Wavefun = list()
   Dwave = list()
   }
 
-  return(list('Scfun'=Scfun, 'Wavefun'=Wavefun, 'Cwave'=Cwave, 'Dwave'=Dwave))
   
+  return(list('Scfun'=Scfun, 'Wavefun'=Wavefun, 'Cwave'=Cwave, 'Dwave'=Dwave))  
 }
